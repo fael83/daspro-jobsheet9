a2e2591612d7b9728c6995e215aa95528f452194
fael83 <rafaellokajaya@gmail.com>
2024-11-15 13:36:29 +0700

    Question Experiment 3

--- ArrayAverageScore13.java
+++ ArrayAverageScore13.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 public class ArrayAverageScore13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            int [] score = new int [10];
-            double total = 0, average;
+            int totalPassed = 0, totalFailed = 0;
+            double total1 = 0,total2=0, average1, average2;
+            System.out.print("Enter the number of student: ");
+            int b = sc.nextInt();
+            int [] score = new int [b];
             for (int i = 0; i < score.length; i++){
-                System.out.print("Enter stident score " + (i + 1) + ": ");
+                System.out.print("Enter the final score " + i + ": ");
                 score[i] = sc.nextInt();
+                if (score[i] > 70) {
+                    totalPassed += score[i];
+                    total1 += 1;
+                }else{
+                    totalFailed += score[i];
+                    total2 += 1;
+                }
             }
-            for (int i = 0; i < score.length; i++){
-                total += score[i];
-            }
-            average = total / score.length;
-            System.out.println("The class average score is " + average);
+            average1 = totalPassed / total1;
+            average2 = totalFailed / total2;
+            System.out.println("The average score of student who passed is " + average1);
+            System.out.println("The average score of student who failed is " + average2);
     }    
 }
