2bfbad5d25de195380870c9d7d7c90b6647ae7f6
fael83 <rafaellokajaya@gmail.com>
2024-11-15 14:42:09 +0700

    Question Experiment 4: Searching

--- LineSearch13.java
+++ LineSearch13.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
 public class LineSearch13 {
     public static void main(String[] args) {
-        int [] arrayInt = {34, 10, 26, 48, 72, 20, 56, 63};
-        int kkey = 20;
+        Scanner sc = new Scanner(System.in);
+        int key;
         int result = 0;
+        System.out.print("Enter the number of array elements: ");
+        int a = sc.nextInt();
+        int [] arrayInt = new int [a]; 
         for (int i = 0; i < arrayInt.length; i++){
-            if (arrayInt[i] == kkey) {
-                result = i;
+            System.out.print("Enter the array element " + i + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+        System.out.print("Enter the key you want to search for: ");
+        key = sc.nextInt();
+        for (int i = 0; i < arrayInt.length; i++){
+            if (arrayInt[i] == key) {
+                System.out.println("The key in the array is located at index position " + i);
                 break;
+            }else if (i == arrayInt.length - 1) {
+                System.out.println("Key not found");
             }
         }
-        System.out.println("The key in the array is located at index position " + result);
     }
 }
